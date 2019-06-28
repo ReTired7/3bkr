8fc7aced038302ac480ba34636c71f00cf1f0c67
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-06-28 05:29:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'pl')) {
     client.user.setGame(argresult);
-      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+      message.channel.send("**:white_check_mark: | .The Playing Status Has Been Changed To : ``"
    + `${argresult}` + "``**")
   } else 
   if (message.content.startsWith(adminprefix + 'wt')) {
@@ -41,5 +41,18 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
+client.on('ready',async () => {//Toxic Codes
+console.log("Starting..");//Toxic Codes
+let g = client.guilds.get("475944692245200898"); // id server
+let c = g.channels.get("589838159630696476");// id channel
+if(c.type === 'voice') {//Toxic Codes
+c.join();//Toxic Codes
+setInterval(() => {//Toxic Codes
+if(!g.me.voiceChannel) c.join();
+}, 1);//Toxic Codes
+} else {//Toxic Codes
+console.log('Failed To Join: \n The Channel Type isn "Listening."')
+}
+});
 
 client.login(process.env.TOKEN);
